421694ca771814724583974dd1c87ece29380354
fluuh <fluhuh@proton.me>
2022-07-13 00:16:57 +0200

    include stdio in tests/main.c

--- tests/main.c
+++ tests/main.c
@@ -1,6 +1,7 @@
+#include <stdio.h>
 #include <tm/ir.h>
 
-int main()
+int main(void)
 {
 	tm_insn insn = TM_INSN(2, 4, 8, 0, 1);
 	printf("%s %s %i, %i, %i\n", tm_type_name(TM_INSN_TYPE(insn)), tm_code_name[TM_INSN_CODE(insn)], TM_INSN_A(insn), TM_INSN_B(insn), TM_INSN_C(insn));
